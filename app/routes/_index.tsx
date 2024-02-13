1e53da8a2dbcbaaab7a71da3b5c96d25b22a2a99
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2024-02-12 23:20:15 -0300

    Corrected workflow file

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -18,7 +18,7 @@ export default function Index() {
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
+            Test
           </a>
         </li>
         <li>
